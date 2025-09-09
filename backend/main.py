4e5eb9ed682831b21aec4555961cebfaef9d453b
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2025-09-08 23:29:27 -0400

    Error fixes

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ import os
 DATABASE_FILE = os.getenv('DATABASE_FILE', 'qaamuus.db')
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
 FRONTEND_URL = os.getenv(
-    'FRONTEND_URL', 'http://somali-to-somali-dictionary.vercel.app/')
+    'FRONTEND_URL', 'http://localhost:3000/')
 
 
 # --- FastAPI App Initialization ---
@@ -22,8 +22,8 @@ app = FastAPI()
 # In production, this will be your Vercel app's URL.
 origins = [
     FRONTEND_URL,
-    "http://somali-to-somali-dictionary.vercel.app/",
     "http://127.0.0.1:3000",
+    "http://localhost:3000"
 ]
 
 app.add_middleware(
--- front-end/app/page.js
+++ front-end/app/page.js
@@ -2,12 +2,7 @@ import Autocomplete from "./components/Autocomplete";
 
 export default function Home() {
   return (
-    // ACCESSIBILITY: Use the <main> tag for the primary content of the page.
-    // Also, declare the primary language of the page as Somali ("so").
-    <main
-      lang="so"
-      className="min-h-screen p-4 bg-gray-100 flex flex-col items-center"
-    >
+    <main className="flex min-h-screen flex-col items-center p-4 bg-gray-100">
       <div className="w-full max-w-md">
         <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">
           Qaamuuska Soomaaliga
