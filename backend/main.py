55b638218a086d19a55d6ccceaaf6115300a4726
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2025-10-04 15:09:02 -0400

    allow cors on development branches

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origin_regex=origin_regex,
+    allow_origin_regex=origin_regex,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
